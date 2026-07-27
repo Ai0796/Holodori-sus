081c63f0ff5eb92c8be37b26fbfab49a14a106b0
Ai0796 <mo.alden25@gmail.com>
2026-07-27 12:25:08 -0400

    seperated out master_data data class

---
+++ lib/language.py
@@ -0,0 +1,45 @@
+            
+from glob import glob
+import os
+import re
+import json
+
+class Lang():
+    
+    def __init__(self, language='Eng'):
+        self.language = language
+        self.langPath = f'master_data/json/Lang*{language}.json'
+        
+        self._files = {}
+        
+        for fp in glob(self.langPath):
+            
+            assetName = re.match(r'Lang(.+)_\w+\.json', os.path.basename(fp)).group(1)
+            
+            self._files[assetName] = fp
+            
+    
+    def __getattr__(self, name):
+        if name in self._files:
+            fp = self._files[name]
+            
+            addDict = {}
+            
+            
+            with open(fp, 'r', encoding='utf-8') as fp:
+                data = json.load(fp)
+                
+            for value in data:
+                if 'id' not in value: continue
+                
+                addDict[value['id']] = value.get('text', None)
+            
+            setattr(self, name, addDict)
+            
+            return getattr(self, name)
+        
+        raise AttributeError(f"'Lang' object has no attribute '{name}'")
+    
+if __name__ == "__main__":
+    lang = Lang()
+    print(lang.Card['la-card_name-00001-3-nrml-0000-00'])
---
+++ lib/master_data.py
@@ -0,0 +1,52 @@
+import json
+from glob import glob
+import os
+import re
+
+masterPath = 'master_data/json/'
+
+class MasterData():
+    
+    def __init__(self):
+        
+        self._files = {}
+        
+        for fp in glob(masterPath + '*.json'):
+            key = os.path.splitext(os.path.basename(fp))[0]
+            
+            ## Don't store language specific files
+            if key.startswith('Lang'):
+                continue
+            
+            self._files[key] = fp
+            # setattr(self, os.path.splitext(os.path.basename(fp))[0], json.load(open(fp, 'r', encoding='utf-8')))
+            
+    def getKeyById(self, asset, key, id, lookup='id'):
+        if not hasattr(self, asset):
+            raise AttributeError(f"'MasterData' object has no attribute '{asset}'")
+        
+        asset_data = getattr(self, asset)
+        
+        for item in asset_data:
+            if item.get(lookup) == id:
+                return item.get(key)
+        
+        return None
+            
+    def __getattr__(self, name):
+        if name in self._files:
+            fp = self._files[name]
+            
+            with open(fp, 'r', encoding='utf-8') as f:
+                return json.load(f)
+            
+            setattr(self, name, json.load(open(fp, 'r', encoding='utf-8')))
+            
+            return getattr(self, name)
+        
+        raise AttributeError(f"'MasterData' object has no attribute '{name}'")
+
+
+if __name__ == "__main__":
+    masterData = MasterData()
+    print(masterData.Card[0])
