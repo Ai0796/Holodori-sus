ff118bd4db7b96856dce2c0b7a24533475ab0359
Ai0796 <mo.alden25@gmail.com>
2026-07-27 12:54:49 -0400

    updated card to read active and passive skill

--- Card/Card.py
+++ Card/Card.py
@@ -1,18 +1,52 @@
-from Card.Skill import Skill
+from Card.ActiveSkill import ActiveSkill
+from Card.PassiveSkill import PassiveSkill
+import json
+from lib.master_data import MasterData
+from lib.language import Lang
 
 class Card():
-    def __init__(self):
+    def __init__(self, master_data: MasterData, lang: Lang):
         self.card_name = None
         self.card_id = None
         self.card_rarity = None
         self.card_type = None
         self.active_skill = None
+        self.passive_skill = None
+        
+        self.master_data = master_data
+        self.lang = lang
         
     def initByDict(self, card_object: dict):
         self.card_id = card_object.get('id', None)
         self.card_rarity = card_object.get('rarity', None)
         self.card_type = card_object.get('attributeType', None)
         
-        if 'livePassiveSkillId' in card_object:
-            self.active_skill = Skill()
+        self.name = self.lang.Card.get(card_object['nameLangId'], None)
+        
+        self.character_name = self.master_data.getKeyById('Character', 'nameEng', card_object['characterId'])
+        
+        self.performance_mult = card_object.get('performancePermilMultiply', None)
+        self.technique_mult = card_object.get('techniquePermilMultiply', None)
+        self.sense_mult = card_object.get('sensePermilMultiply', None)
+        
+        if 'liveActiveSkillId' in card_object:
+            self.active_skill = ActiveSkill(self.master_data, self.lang)
             self.active_skill.initByName(card_object['liveActiveSkillId'])
+            
+        if 'livePassiveSkillId' in card_object:
+            self.passive_skill = PassiveSkill(self.master_data, self.lang)
+            self.passive_skill.initByName(card_object['livePassiveSkillId'])
+            
+            print(f"Loaded passive skill: {self.passive_skill} for card {self.character_name} ({self.card_name})")
+            
+    def initById(self, card_id):
+        cards = self.master_data.Card
+            
+        for card in cards:
+            if card['id'] == card_id:
+                self.initByDict(card)
+                break
+            
+        
+    def __str__(self):
+        return f"Card({self.card_id}: {self.character_name} - {self.card_name}, {self.card_rarity}, {self.card_type}"
